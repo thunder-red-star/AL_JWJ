5cfc0a044c051cacb5d6af1be96e3977d3aac34c
thunder-red-star <wvongphanith30@stuy.edu>
2021-12-10 12:25:55 -0500

    finish binary searcg

--- Utils.java
+++ Utils.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Utils {
     // average of two integers
     public static int average(int a, int b) {
@@ -9,7 +11,7 @@ public class Utils {
         int high = arr.size() - 1;
         while (low <= high) {
             if (value > arr.get(average(low, high))) {
-                return average(low, high);
+                low = average(low, high);
             }
             if (value < arr.get(average(low, high))) {
                 high = average(low, high) - 1;
@@ -20,5 +22,67 @@ public class Utils {
         }
     }
 
-
+    public static void main(String[] args) {
+        ArrayList<integer> arr = new ArrayList<integer>();
+        arr.add(1);
+        arr.add(2);
+        arr.add(3);
+        arr.add(4);
+        arr.add(5);
+        arr.add(6);
+        arr.add(7);
+        arr.add(8);
+        arr.add(9);
+        arr.add(10);
+        arr.add(11);
+        arr.add(12);
+        arr.add(13);
+        arr.add(14);
+        arr.add(15);
+        arr.add(16);
+        arr.add(17);
+        arr.add(18);
+        arr.add(19);
+        arr.add(20);
+        arr.add(21);
+        arr.add(22);
+        arr.add(23);
+        arr.add(24);
+        arr.add(25);
+        arr.add(26);
+        arr.add(27);
+        arr.add(28);
+        arr.add(29);
+        arr.add(30);
+        arr.add(31);
+        arr.add(32);
+        arr.add(33);
+        arr.add(34);
+        arr.add(35);
+        arr.add(36);
+        arr.add(37);
+        arr.add(38);
+        arr.add(39);
+        arr.add(40);
+        arr.add(41);
+        arr.add(42);
+        arr.add(43);
+        arr.add(44);
+        arr.add(45);
+        arr.add(46);
+        arr.add(47);
+        arr.add(48);
+        arr.add(49);
+        arr.add(50);
+        arr.add(51);
+        arr.add(52);
+        arr.add(53);
+        arr.add(54);
+        arr.add(55);
+        arr.add(56);
+        arr.add(57);
+        arr.add(58);
+        arr.add(59);
+        binarySearch(arr, 50);
+    }
 }
