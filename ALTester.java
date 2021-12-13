44d2ef9c621361554389e177684296c1165aed9d
thunder-red-star <wvongphanith30@stuy.edu>
2021-12-13 12:05:20 -0500

    work harderer

---
+++ ALTester.java
@@ -0,0 +1,15 @@
+public class ALTester {
+    public static void main(String[] args) {
+        OrderedArrayList list = new OrderedArrayList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(84);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+        list.add(8);
+        list.add(9);
+        list.add(10);
+    }
+}
