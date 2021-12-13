cf86a8c52b180a6b0c9edfa7f277c64d7df6ee40
JeffordShau <90117913+JeffordShau@users.noreply.github.com>
2021-12-13 15:28:22 -0500

    Update ALTester.java

--- ALTester.java
+++ ALTester.java
@@ -10,7 +10,28 @@ public class ALTester {
         list.add(7);
         list.add(8);
         list.add(9);
-        list.add(10);
+        list.add(75);
+        list.add(12);
+        list.add(66);
+        list.add(23);
+        list.add(24);
+        list.add(25);
+        list.add(26);
+        list.add(28);
+        list.add(29);
+        list.add(30);
+        list.add(32);
+        list.add(35);
+        list.add(39);
+        list.add(37);
+        System.out.println(list.toString());
+        list.remove(12);
+        System.out.println(list.toString());
+        list.remove(20);
+        System.out.println(list.toString());
+        list.set(3, 39);
+        System.out.println(list.toString());
+        list.set(5, 18);
         System.out.println(list.toString());
     }
 }
